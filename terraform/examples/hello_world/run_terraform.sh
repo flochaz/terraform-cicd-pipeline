fa7d7037ec190a86958af02fa1e6c1891e8ccdcd
Florian CHAZAL <chazalf@amazon.com>
2021-07-08 15:23:31 +0200

    export local creds

--- terraform/examples/hello_world/run_terraform.sh
+++ terraform/examples/hello_world/run_terraform.sh
@@ -2,11 +2,10 @@
 
 ACTION=${1} # destroy / plan / apply
 
-APPLY=
 
 if ( [[ -z ${AWS_ACCESS_KEY_ID} ]] || [[ -z ${AWS_SECRET_ACCESS_KEY} ]] ) && [[ -z ${AWS_PROFILE} ]]; then
-    echo "[ERROR] Missing AWS credentials variables"
-    exit 1
+    curl 169.254.170.2$AWS_CONTAINER_CREDENTIALS_RELATIVE_URI | jq 'to_entries | [ .[] | select(.key | (contains("Expiration") or contains("RoleArn"))  | not) ] |  map(if .key == "AccessKeyId" then . + {"key":"AWS_ACCESS_KEY_ID"} else . end) | map(if .key == "SecretAccessKey" then . + {"key":"AWS_SECRET_ACCESS_KEY"} else . end) | map(if .key == "Token" then . + {"key":"AWS_SESSION_TOKEN"} else . end) | map("export \(.key)=\(.value)") | .[]' -r > /tmp/aws_cred_export.txt # work around https://github.com/hashicorp/terraform/issues/8746
+    source /tmp/aws_cred_export.txt
 fi
 
 TFVARS_FILE="inventories/variables.tfvars"
