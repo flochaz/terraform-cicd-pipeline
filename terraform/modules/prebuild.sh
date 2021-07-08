f174b6f9740c037a27d615772b217c6b38761190
Florian CHAZAL <chazalf@amazon.com>
2021-07-08 17:02:05 +0200

    back to creds in run_terraform.sh

--- terraform/modules/prebuild.sh
+++ terraform/modules/prebuild.sh
@@ -2,8 +2,6 @@
 set -e
 
 if [[ -z  ${TEST_ACCOUNT_ASSUMED_ROLE} ]]; then
-  curl 169.254.170.2$AWS_CONTAINER_CREDENTIALS_RELATIVE_URI | jq 'to_entries | [ .[] | select(.key | (contains("Expiration") or contains("RoleArn"))  | not) ] |  map(if .key == "AccessKeyId" then . + {"key":"AWS_ACCESS_KEY_ID"} else . end) | map(if .key == "SecretAccessKey" then . + {"key":"AWS_SECRET_ACCESS_KEY"} else . end) | map(if .key == "Token" then . + {"key":"AWS_SESSION_TOKEN"} else . end) | map("export \(.key)=\(.value)") | .[]' -r > /tmp/aws_cred_export.txt # work around https://github.com/hashicorp/terraform/issues/8746
-  source /tmp/aws_cred_export.txt
   echo "[INFO] Local Deployment"
   exit 0
 fi
